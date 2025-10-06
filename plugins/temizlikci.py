0d170ac09910e9a604c730cdc8469b88eed4f45f
Ekrem Kartal <24720760010@mersin.edu.tr>
2025-10-06 23:50:32 +0300

    Update temizlikci.py

--- plugins/temizlikci.py
+++ plugins/temizlikci.py
@@ -1,7 +1,7 @@
 import os
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command('diskx1'))
+@Client.on_message(filters.command('diskx'))
 async def disksil(bot, message):
     try:
         silkomut = "rm -rf DOWNLOADS"
